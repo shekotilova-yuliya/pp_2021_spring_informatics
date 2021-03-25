abecc5942b9adaf024e20e368d06b3c1442a99cf
shekotilova-yuliya <shekotilova.yuliya@mail.ru>
2021-03-25 22:56:10 +0300

    task_1

---
+++ modules/task_1/schekotilova_ju_radix_merge_double/main.cpp
@@ -0,0 +1,96 @@
+// Copyright 2021 Schekotilova Julia
+
+#include <gtest/gtest.h>
+#include "./radix_merge_double.h"
+
+TEST(TEST_SEQ, WROGNG_SIZE) {
+  int size = -10, res = 0;
+  double* arr = nullptr;
+
+  if (size > 0) {
+    arr = reinterpret_cast<double*>(malloc(sizeof(double) * size));
+  } else {
+    res = -1;
+  }
+  res = generateArray(arr, size);
+  res = radixSort(arr, size);
+  res = checker(arr, size);
+  if (arr) {
+    free(arr);
+    arr = nullptr;
+  }
+  ASSERT_EQ(res, -1);
+}
+
+TEST(TEST_SEQ, ZERO_SIZE) {
+   int size = 0, res = 0;
+  double* arr = nullptr;
+
+  if (size > 0) {
+    arr = reinterpret_cast<double*>(malloc(sizeof(double) * size));
+  } else {
+    res = -1;
+  }
+  res = generateArray(arr, size);
+  res = radixSort(arr, size);
+  res = checker(arr, size);
+  if (arr) {
+    free(arr);
+    arr = nullptr;
+  }
+  ASSERT_EQ(res, -1);
+}
+
+TEST(TEST_SEQ, SIZE_10) {
+  int size = 10, res = 0;
+  double* arr = reinterpret_cast<double*>(malloc(sizeof(double) * size));
+  res = generateArray(arr, size);
+  res = radixSort(arr, size);
+  res = checker(arr, size);
+  free(arr);
+  arr = nullptr;
+  ASSERT_EQ(res, 0);
+}
+
+TEST(TEST_SEQ, SIZE_100) {
+  int size = 100, res = 0;
+  double* arr = nullptr;
+
+  if (size > 0) {
+    arr = reinterpret_cast<double*>(malloc(sizeof(double) * size));
+  } else {
+    res = -1;
+  }
+  res = generateArray(arr, size);
+  res = radixSort(arr, size);
+  res = checker(arr, size);
+  if (arr) {
+    free(arr);
+    arr = nullptr;
+  }
+  ASSERT_EQ(res, 0);
+}
+
+TEST(TEST_SEQ, SIZE_1000) {
+  int size = 1000, res = 0;
+  double* arr = nullptr;
+
+  if (size > 0) {
+    arr = reinterpret_cast<double*>(malloc(sizeof(double) * size));
+  } else {
+    res = -1;
+  }
+  res = generateArray(arr, size);
+  res = radixSort(arr, size);
+  res = checker(arr, size);
+  if (arr) {
+    free(arr);
+    arr = nullptr;
+  }
+  ASSERT_EQ(res, 0);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ modules/task_1/schekotilova_ju_radix_merge_double/radix_merge_double.cpp
@@ -0,0 +1,130 @@
+// Copyright 2021 Schekotilova Julia
+
+#include "../../../modules/task_1/schekotilova_ju_radix_merge_double/radix_merge_double.h"
+
+int mergingArrays(double* arr, double* arr_, int size, int size_, double* result) {
+  int a = 0;
+  int sz = size - 1;
+  if (size < 0 || size_ < 0 || result == nullptr || arr_ == nullptr || arr == nullptr) {
+    return - 1;
+  } else {
+    for (int i = sz; i >= 0; i--) {
+    result[a] = arr[i];
+    a++;
+    }
+    for (int j = 0; j < size_; j++) {
+      result[a] = arr_[j];
+      a++;
+    }
+  return 0;
+  }
+}
+
+void countingSort(double* in, double* out, int byteVal, int size) {
+  unsigned char* arr = (unsigned char*)in;
+  int count[256], value, i;
+  memset(count, 0, sizeof(int) * 256);
+  for (int j = 0; j < size; j++) {
+    count[arr[8 * j + byteVal]]++;
+  }
+  for (i = 0; i < 256; i++) {
+    if (count[i] != 0) {
+      break;
+    }
+  }
+  value = count[i];
+  count[i] = 0;
+  i++;
+  for (i; i < 256; i++) {
+    int tmp = count[i];
+    count[i] = value;
+    value += tmp;
+  }
+
+  for (int j = 0; j < size; j++) {
+    out[count[arr[8 * j + byteVal]]] = in[j];
+    count[arr[8 * j + byteVal]]++;
+  }
+}
+
+int unsgnRadixSort(double* arr, int size) {
+  double* outArr = reinterpret_cast<double*>(malloc(sizeof(double) * size));
+  int num = 4;
+  if (size < 1) {
+    return 0;
+  } else if (size < 0 || arr == nullptr || outArr == nullptr) {
+    return -1;
+  } else {
+    for (int i = 0; i < num; i++) {
+      countingSort(arr, outArr, 2 * i, size);
+      countingSort(outArr, arr, 2 * i + 1, size);
+    }
+    if (outArr) {
+      free(outArr);
+      outArr = nullptr;
+    }
+    return 0;
+  }
+}
+
+
+int radixSort(double* arr, int size) {
+  double* pArr = nullptr, * nArr = nullptr;
+  int pSize = 0, nSize = 0;
+  int res = 0;
+  pArr = reinterpret_cast<double*>(malloc(sizeof(double) * size));
+  nArr = reinterpret_cast<double*>(malloc(sizeof(double) * size));
+  if ((size < 1) || (arr == nullptr)) {
+    return -1;
+  } else if ((pArr == nullptr) || (nArr == nullptr)) {
+    return -1;
+  } else {
+    for (int i = 0; i < size; i++) {
+      if (arr[i] >= 0) {
+        pArr[pSize] = arr[i];
+        pSize++;
+      } else {
+        nArr[nSize] = arr[i];
+        nSize++;
+      }
+    }
+
+    res = unsgnRadixSort(nArr, nSize);
+    res = unsgnRadixSort(pArr, pSize);
+    res = mergingArrays(nArr, pArr, nSize, pSize, arr);
+    if (nArr) {
+      free(nArr);
+      nArr = nullptr;
+    }
+    if (pArr) {
+      free(pArr);
+      pArr = nullptr;
+    }
+    return res;
+  }
+}
+
+int generateArray(double* arr, int size) {
+  double b = 100.0;
+  double a = 0.0;
+  for (int i = 0; i < size; i++) {
+    arr[i] = static_cast<double>(std::rand()) * (b - a + 1) / RAND_MAX + a;
+  }
+  return 0;
+}
+
+int checker(double* arr, int size) {
+  int sz = size - 1;
+  if (size < 1 || arr == nullptr) {
+    return -1;
+  } else {
+    for (int i = 0; i < sz; i++) {
+      if (arr[i] <= arr[i + 1]) {
+        i++;
+      } else {
+        return -1;
+      }
+    }
+    return 0;
+  }
+}
