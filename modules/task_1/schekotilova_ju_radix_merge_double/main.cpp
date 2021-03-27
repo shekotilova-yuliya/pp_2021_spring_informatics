c2bfe85a2c8e08fe3b8d51ed98a60aa498aa5d10
shekotilova-yuliya <43138513+shekotilova-yuliya@users.noreply.github.com>
2021-03-27 16:41:54 +0300

    Update main.cpp

--- modules/task_1/schekotilova_ju_radix_merge_double/main.cpp
+++ modules/task_1/schekotilova_ju_radix_merge_double/main.cpp
@@ -6,50 +6,56 @@
 TEST(TEST_SEQ, SIZE_5) {
   int size = 5;
   double* arr = new double[size] { 1.65, 5.5582, 29.9, 9.155, 52.864 };
-  bool result = false;
+  bool res = false;
   radixSort(arr, size);
-  result = checker(arr, size);
-  ASSERT_EQ(true, result);
+  res = checker(arr, size);
+  ASSERT_EQ(true, res);
+  delete[] arr;
 }
 
 TEST(TEST_SEQ, SIZE_1000) {
   int size = 1000;
   double* arr = new double[size];
   for (int i = 0; i < size; i++) arr[i] = (size - i) * 0.001 + (i + size / 2);
-  bool result = false;
+  bool res = false;
   radixSort(arr, size);
-  result = checker(arr, size);
-
-  ASSERT_EQ(true, result);
+  res = checker(arr, size);
+  ASSERT_EQ(true, res);
+  delete[] arr;
 }
 
 TEST(TEST_SEQ, SORT_RANDOM) {
   int size = 5;
   double* arr = new double[size];
-  bool result = false;
+  bool res = false;
   arr = generateArray(arr, size);
   radixSort(arr, size);
-  result = checker(arr, size);
-  ASSERT_EQ(true, result);
+  res = checker(arr, size);
+  ASSERT_EQ(true, res);
+  delete[] arr;
 }
+
 TEST(TEST_SEQ, SORT_SIZE100) {
   int size = 100;
   double* arr = new double[size];
   for (int i = 0; i < size; i++) arr[i] = (size - i) * 0.01 + (i + size / 2);
-  bool result = false;
+  bool res = false;
   radixSort(arr, size);
-  result = checker(arr, size);
-  ASSERT_EQ(true, result);
+  res = checker(arr, size);
+  ASSERT_EQ(true, res);
+  delete[] arr;
 }
 
 TEST(TEST_SEQ, CORRECT_SORT) {
   int size = 6;
-  double* result = new double[size] {1.23, 5.6, 43.44, 50.21, 68.8802, 100.2364};
+  double* resArr = new double[size] {1.23, 5.6, 43.44, 50.21, 68.8802, 100.2364};
   double* arr = new double[size] {43.44, 5.6, 50.21, 1.23, 100.2364, 68.8802};
   radixSort(arr, size);
   for (int i = 0; i < size; i++) {
-    ASSERT_NEAR(result[i], arr[i], 0.01);
+    ASSERT_NEAR(resArr[i], arr[i], 0.01);
   }
+  delete[] resArr;
+  delete[] arr;
 }
 
 int main(int argc, char** argv) {
